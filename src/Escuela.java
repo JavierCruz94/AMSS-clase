61e57f9caad2c7a3d7341af3f2efc10ce4854e1b
elielr01 <eli.elr01@gmail.com>
2015-11-06 10:59:06 -0600

    Escuela.java listo

--- src/Escuela.java
+++ src/Escuela.java
@@ -1,3 +1,7 @@
+
+import java.util.LinkedList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,6 +13,68 @@
  * @author Javi Cruz
  *@author Eli LInares
  */
-class Escuela {
+public class Escuela {
     
+    String nombre;
+    String city;
+    String direccion;
+    String telefono;
+    LinkedList<Departamento> departamentos;
+    LinkedList<Estudiante> estudiantes;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public LinkedList<Departamento> getDepartamentos() {
+        return departamentos;
+    }
+
+    public void setDepartamentos(LinkedList<Departamento> departamentos) {
+        this.departamentos = departamentos;
+    }
+    
+    public void addDepartamento(Departamento departamento) {        
+        this.departamentos.addLast(departamento);
+    }
+
+    public LinkedList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(LinkedList<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public void addEstudiante(Estudiante estudiante) {
+        this.estudiantes.addFirst(estudiante);
+    }
 }
